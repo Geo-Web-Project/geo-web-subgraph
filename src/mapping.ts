28cea5c54ebd87a81b6fa5500acdfd27c1f25dfc
Cody Hatfield <cody.hatfield@me.com>
2020-11-03 13:44:26 -0800

    More direct GeoJSON mapping, include all coordinates

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,84 +1,107 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import { GeoWebCoordinate } from "as-geo-web-coordinate/assembly"
+import { DirectionPath, GeoWebCoordinate, GeoWebCoordinatePath, u256 } from "as-geo-web-coordinate/assembly"
 import {
   GeoWebParcel,
   MintGeoWebParcel
 } from "../generated/GeoWebParcel/GeoWebParcel"
-import { LandParcel, GeoJSONPolygon, GeoJSONCoordinate } from "../generated/schema"
+import { LandParcel, GeoJSONMultiPolygon, GeoJSONPolygon, GeoJSONCoordinate } from "../generated/schema"
 import { BigDecimal } from '@graphprotocol/graph-ts'
-import { log } from '@graphprotocol/graph-ts'
 
 export function handleMintGeoWebParcel(event: MintGeoWebParcel): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
   let entity = LandParcel.load(event.params._id.toHex())
-  let polygonEntity = GeoJSONPolygon.load(event.params._id.toHex())
+  let multiPolyEntity = GeoJSONMultiPolygon.load(event.params._id.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (entity == null) {
     entity = new LandParcel(event.params._id.toHex())
+    entity.type = "Feature"
   }
 
-  if (polygonEntity == null) {
-    polygonEntity = new GeoJSONPolygon(event.params._id.toHex())
+  if (multiPolyEntity == null) {
+    multiPolyEntity = new GeoJSONMultiPolygon(event.params._id.toHex())
+    multiPolyEntity.type = "MultiPolygon"
   }
 
   let contract = GeoWebParcel.bind(event.address)
   let parcel = contract.getLandParcel(event.params._id)
+
+  let numPaths = parcel.value1.length
+  let paths: BigInt[] = parcel.value1
+
+  let coordIDs = new Array<string>(numPaths*124)
+
+  let currentCoord = <u64>Number.parseInt(parcel.value0.toHex().slice(2), 16)
+  let currentPath: u256
+  let p_i = 0
+  let i = 0
+  if (numPaths > 0) {
+    currentPath = u256.fromUint8ArrayLE(paths[p_i])
+  }
+  do {
+    savePolygon(currentCoord)
+    coordIDs[i] = currentCoord.toString()
+    i += 1
+
+    let hasNext = GeoWebCoordinatePath.hasNext(currentPath)
+
+    let directionPath: DirectionPath
+    if (!hasNext) {
+      // Try next path
+      p_i += 1
+      if (p_i >= numPaths) {
+        break;
+      }
+      currentPath = u256.fromUint8ArrayLE(paths[p_i])
+    }
+
+    directionPath = GeoWebCoordinatePath.nextDirection(currentPath)
+    currentPath = directionPath.path
+
+    // Traverse to next coordinate
+    currentCoord = GeoWebCoordinate.traverse(currentCoord, directionPath.direction)
+  } while (true)
+
+
+  multiPolyEntity.coordinates = coordIDs
+  entity.geometry = multiPolyEntity.id
   
-  let coords = GeoWebCoordinate.to_gps_hex(parcel.value0.toHex().slice(2)).map<BigDecimal[]>((v1: string[]) => {
-    return v1.map<BigDecimal>((v2: string) => {
-      return BigDecimal.fromString(v2)
-    })
+  multiPolyEntity.save()
+  entity.save()
+}
+
+function savePolygon(gwCoord: u64): void {
+  let polygonEntity = GeoJSONPolygon.load(gwCoord.toString())
+
+  if (polygonEntity == null) {
+    polygonEntity = new GeoJSONPolygon(gwCoord.toString())
+    polygonEntity.type = "Polygon"
+  }
+
+  let coords = GeoWebCoordinate.to_gps(gwCoord).map<BigDecimal>((v: f64) => {
+    return BigDecimal.fromString(v.toString())
   })
 
   let coordIDs = new Array<string>(coords.length)
 
-  for (let i = 0; i < coords.length; i++) {
-    let coord = coords[i];
-    let coordID = coord[0].toString() + ";" + coord[1].toString()
+  for (let i = 0; i < coords.length; i += 2) {
+    let lon = coords[i];
+    let lat = coords[i+1];
+    let coordID = lon.toString() + ";" + lat.toString()
     let coordinateEntity = GeoJSONCoordinate.load(coordID)
     if (coordinateEntity == null) {
       coordinateEntity = new GeoJSONCoordinate(coordID)
     }
 
-    coordinateEntity.lon = coord[0]
-    coordinateEntity.lat = coord[1]
+    coordinateEntity.lon = lon
+    coordinateEntity.lat = lat
 
     coordIDs[i] = coordinateEntity.id
     coordinateEntity.save()
   }
   
-  polygonEntity.type = "Polygon"
   polygonEntity.coordinates = coordIDs
-
-  entity.polygon = polygonEntity.id
-  
   polygonEntity.save()
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.MINTER_ROLE(...)
-  // - contract.availabilityIndex(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.getRoleMember(...)
-  // - contract.getRoleMemberCount(...)
-  // - contract.hasRole(...)
-  // - contract.getLandParcel(...)
 }
